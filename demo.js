f5ccf99ea4d001bc391c46f3a5a810dd4ae1fdfd
dangrammer <danieljromans@gmail.com>
2020-02-13 13:11:35 -0500

    added find all pairs solution

--- demo.js
+++ demo.js
@@ -136,10 +136,29 @@ function superStack(operations) {
 
 // sock merchant (find all pairs) ////////////////////
 
+function sockMerchant(n, ar) {
+  let likeSocks = {}
+  let pairs = 0
+  
+  for (let i = 0; i < n; i++) {
+    likeSocks[ar[i]] ? likeSocks[ar[i]] += 1 : likeSocks[ar[i]] = 1
+  }
+  
+  for (let sock in likeSocks) {
+    if (likeSocks[sock] % 2 === 0) {
+      pairs += likeSocks[sock] / 2
+    } else {
+      pairs += (likeSocks[sock] - 1) / 2 
+    }
+  }
+  // console.log(pairs)
+  return pairs
+}
+
 // let n = 9
 // let ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+// sockMerchant(n, ar)
 
-// function sockMerchant(n, ar) {
 
 
-// }
+/////////////////////////////////////////////////
