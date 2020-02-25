c9ed8c0e2a23bd9854304389439402f5a192c089
dangrammer <danieljromans@gmail.com>
2020-02-24 19:04:12 -0500

    added jumping on clouds solution

--- solutions.js
+++ solutions.js
@@ -212,5 +212,28 @@ function countingValleys(n, s) {
   return valleys
 }
 
-let path = 'DDUUUUDDDDUU'
-countingValleys(12, path)
+// let path = 'DDUUUUDDDDUU'
+// countingValleys(12, path)
+
+
+
+// jumping on the clouds ////////////////////////////////////////
+
+function jumpingOnClouds(c) {
+  let jumpCount = 0
+  let i = 0
+    
+  while(i < c.length - 1) { 
+    if (i + 2 < c.length && c[i + 2] === 0) {
+      jumpCount += 1
+      i += 2
+    } else {
+      jumpCount += 1
+      i += 1
+    }     
+  }
+  return jumpCount
+}
+
+// c = [0, 0, 1, 0, 0, 1, 0]
+// jumpingOnClouds(c)
