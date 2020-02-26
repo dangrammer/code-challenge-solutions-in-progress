314f9df6dad4aab5153cbf33bb31ab69abc7a12c
dangrammer <danieljromans@gmail.com>
2020-02-25 20:42:39 -0500

    added closed paths solution

--- solutions.js
+++ solutions.js
@@ -275,3 +275,22 @@ function repeatedString(s, n) {
 // let s = 'aba'
 // let n = 10
 // repeatedString(s, n)
+
+
+
+// closed paths /////////////////////////////////////////////
+
+function closedPaths(num) {
+  const n = num.toString()
+  const cPaths = ['0', '4', '6', '9']
+  let count = 0
+  
+  for (let i = 0; i < n.length; i++) {
+    if (cPaths.includes(n[i])) count += 1
+    if (n[i] === '8') count += 2
+  }
+  return count
+}
+
+// let n = 1288
+// closedPaths(n)
