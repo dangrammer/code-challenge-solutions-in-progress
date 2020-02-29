0e9b558bc54a2eea9b7d76931ff6f81366b517a4
dangrammer <danieljromans@gmail.com>
2020-02-28 19:36:27 -0500

    added array: minimum bribes solution

--- solutions.js
+++ solutions.js
@@ -347,4 +347,30 @@ function rotLeft(a, d) {
 // let d = 4
 // rotLeft(a, d)
 
-//
+
+
+// array: minimum bribes /////////////////////////////////
+
+function minimumBribes(q) {
+  let swaps = 0
+
+  for (let i = q.length - 1; i >= 0; i--) {
+    let bribes = q[i] - (i + 1)
+    let spot = q[i] - 2 > 0 ? q[i] - 2 : 0
+
+    if (bribes > 2) {
+      console.log('Too chaotic')
+      return
+    }
+
+    for (let j = spot; j < i; j++) {
+      if (q[j] > q[i]) swaps++
+    }
+  }
+
+  console.log(swaps)
+}
+
+// let q = [2, 1, 5, 3, 4]
+// let q = [2, 5, 1, 3, 4]
+minimumBribes(q)
