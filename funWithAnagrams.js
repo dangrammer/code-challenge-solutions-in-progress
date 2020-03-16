1f4bd6e8cb7873a8d6f58d72beacede4632f1c2c
dangrammer <danieljromans@gmail.com>
2020-03-16 18:47:36 -0400

    added solution for funWithAnagrams

--- funWithAnagrams.js
+++ funWithAnagrams.js
@@ -1,43 +1,56 @@
-function funWithAnagrams(text) {
-  const result = []
+// This solution traverses input array in linear order, which
+// allows an anagram of an element to appear later in the sequence.
+// To my understanding, the problem statement required this.
+// However, at least one example solution did not include
+// a later element which was an anagram of the first element
+// yet not an anagram of the element which preceded it,
+// which should have been valid and included in the returned output.
+// It was a point of confusion... 
 
-  result.push(text.shift())
+function funWithAnagrams(text) {
+  const result = [text[0]]
+ 
   while (text.length > 0) {
-    if (text[0].split('').sort().join('') === result.pop().split('').sort().join('')) {
-      text.shift()
+    let head = result[result.length - 1]
+    let anaG = text[0]
+
+    if (isAnagram(anaG, head)) {
+      text.splice(anaG, 1)
     } else {
       result.push(text.shift())
     }
   }
 
-  console.log(result.sort())
   return(result.sort())
 }
+
+
+// helper function to check if two strings are anagrams
+
+function isAnagram(s1, s2) {
+  const hGram = {}
+
+  if (s1.length !== s2.length) return false
+   
+  for (let i = 0; i < s1.length; i++) {
+    hGram[s1[i]] ? hGram[s1[i]]++ : hGram[s1[i]] = 1
+  }
+
+  for (let i = 0; i < s2.length; i++) {
+    if (hGram[s2[i]] > 0) {
+      hGram[s2[i]]--
+    } else {
+      return false
+    }
+  }
+
+  return true
+}
+
+
+// invoke function
 
 const text = ['code', 'aaagmnrs', 'anagrams', 'doce']
 funWithAnagrams(text)
 
-// function isAnagram(s1, s2) {
-//   const hGram = {}
-
-//   for (let i = 0; i < s1.length; i++) {
-//     hGram[s1[i]] ? hGram[s1[i]]++ : hGram[s1[i]] = 1
-//   }
-
-//   console.log(hGram)
-  
-//   for (let i = 0; i < s2.length; i++) {
-//     console.log(s2[i])
-//     if (hGram[s2[i]]) {
-//       if (hGram[s2[i]] > 0) hGram[s2[i]]--
-//     } else {
-//       console.log(false)
-//       return false
-//     } 
-//   }
-
-//   console.log(true)
-// }
-
-// isAnagram('code', 'doc')
-
+// => ['aaagmnrs', 'code', 'doce']
