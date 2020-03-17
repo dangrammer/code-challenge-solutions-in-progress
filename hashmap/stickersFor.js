c6293cf41236801acd47691bcf156f01121791bf
dangrammer <danieljromans@gmail.com>
2020-03-17 15:09:18 -0400

    refactored solution for stickersFor

--- hashmap/stickersFor.js
+++ hashmap/stickersFor.js
@@ -1,33 +1,56 @@
 /////////// * FIRST DRAFT * ///////////
 
-function stickers_for(phrase) {
+// function stickers_for(phrase) {
+//   const hGram = {}
+//   const insta = 'instagram'
+
+//   // build histogram to track values of i, n, s, t, a, g, r, m
+//   for (let i = 0; i < insta.length; i++) {
+//     if (!hGram[insta[i]]) hGram[insta[i]] = 0
+//   }
+
+//   // produce value count of phrase characters in histogram
+//   for (let i = 0; i < phrase.length; i++) {
+//     hGram[phrase[i]] ? hGram[phrase[i]]++ : hGram[phrase[i]] = 1
+//   }
+
+//   // set value of stickers needed based on 'a', halved because each sticker has two 'a's
+//   const aVal = Math.ceil(hGram['a'] / 2)
+
+//   // remove consideration of 'a' value and collect max value of remaining set
+//   hGram['a'] = 0
+//   const maxVal = Math.max(...Object.values(hGram))
+
+//   // return greater of aVal or maxVal
+//   console.log(Math.max(aVal, maxVal))
+//   return Math.max(aVal, maxVal)
+// }
+
+/////////// * SECOND DRAFT * ///////////
+
+function stickersFor(phrase) {
   const hGram = {}
-  const insta = 'instagram'
+  let max = 0
 
-  // build histogram to track values of i, n, s, t, a, g, r, m
-  for (let i = 0; i < insta.length; i++) {
-    if (!hGram[insta[i]]) hGram[insta[i]] = 0
-  }
-
-  // produce value count of phrase characters in histogram
   for (let i = 0; i < phrase.length; i++) {
-    hGram[phrase[i]] ? hGram[phrase[i]]++ : hGram[phrase[i]] = 1
+    if (phrase[i] !== ' ') {
+      hGram[phrase[i]] ? hGram[phrase[i]]++ : hGram[phrase[i]] = 1
+    }
   }
 
-  // set value of stickers needed based on 'a', halved because each sticker has two 'a's
-  const aVal = Math.ceil(hGram['a'] / 2)
+  for (let l in hGram) {
+    if (l === 'a') {
+      max = Math.max(max, Math.ceil(hGram[l] / 2))
+    } else {
+      max = Math.max(max, hGram[l])
+    }
+  }
 
-  // remove consideration of 'a' value and collect max value of remaining set
-  hGram['a'] = 0
-  const maxVal = Math.max(...Object.values(hGram))
-
-  // return greater of aVal or maxVal
-  console.log(Math.max(aVal, maxVal))
-  return Math.max(aVal, maxVal)
+  console.log(`${max} ${max === 1 ? 'sticker' : 'stickers'} needed to spell out "${phrase}"`)
+  return max
 }
 
-// const phrase = 'tiara' // 1
-// const phrase = 'artisan martians' // 2
-const phrase = 'tagaaaaaming giant gnats' // 3
-
-stickers_for(phrase)
+// const phrase = 'tiara' // => 1
+// const phrase = 'artisan martians' // => 2
+const phrase = 'taming giant gnats' // => 3
+stickersFor(phrase)
