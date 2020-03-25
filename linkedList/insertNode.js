cdb106fe2028deb9089a4405586e8fa968677935
dangrammer <danieljromans@gmail.com>
2020-03-25 14:28:12 -0400

    added insertNode solution

---
+++ linkedList/insertNode.js
@@ -0,0 +1,64 @@
+//////////// *** FIRST DRAFT *** ////////////
+
+function insertNodeAtPosition(head, data, position) {
+  let i = 0
+  let node
+
+  if (!head) {
+    node = {data, next: null}
+    return node
+  }
+  
+  node = head
+
+  while (node) {
+    if (i === position - 1) {
+      const prevNode = node
+      const newNode = {data, next: prevNode.next}
+      prevNode.next = newNode
+      return head
+   }
+   node = node.next
+   i++
+  }
+
+  return null
+}
+
+//////////// *** SECOND DRAFT *** ////////////
+
+// function insertNodeAtPosition(head, data, position) {
+//   let i = 0
+//   let node = head
+
+//   while (node) {
+//     if (i === position - 1) {
+//       const prevNode = node
+//       const newNode = {data, next: prevNode.next}
+//       prevNode.next = newNode
+//       return head
+//    }
+//    node = node.next
+//    i++
+//   }
+
+//   return {data, next: null}
+// }
+
+//////////// *** THIRD DRAFT *** ////////////
+
+// function insertNodeAtPosition(head, data, position) {
+//   let i = 0
+//   let node = head
+
+//   while (node) {
+//     if (i === position - 1) {
+//       const prevNode = node
+//       const newNode = {data, next: prevNode.next}
+//       prevNode.next = newNode
+//       return head
+//     }
+//     node = node.next
+//     i++
+//   }
+// }
