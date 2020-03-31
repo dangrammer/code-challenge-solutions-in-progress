35336aa0faf7ca6f8df6d0692c11dd1c7a3ff399
dangrammer <danieljromans@gmail.com>
2020-03-30 21:05:46 -0400

    added solution for make anagram

---
+++ string/makeAnagram.js
@@ -0,0 +1,28 @@
+function makeAnagram(a, b) {
+  const hGram = {}
+  const str1 = a.length >= b.length ? a : b
+  const str2 = a.length < b.length ? a : b
+  let count = 0
+
+  for (let i = 0; i < str1.length; i++) {
+    hGram[str1[i]] ? hGram[str1[i]]++ : hGram[str1[i]] = 1
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    hGram[str2[i]] > 0 ? hGram[str2[i]]-- : count++
+  }
+
+  // for (let key in hGram) {
+  //   count += hGram[key]
+  // }
+
+  count += Object.values(hGram).reduce((a, b) => a + b)
+  
+  console.log(count)
+  return count
+}
+
+const a = 'cde'
+const b = 'abc'
+makeAnagram(a, b)
+//=> 2
