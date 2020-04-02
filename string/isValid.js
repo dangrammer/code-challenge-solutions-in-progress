1cf4059c357211693523cf919f5c56cd92048afa
dangrammer <danieljromans@gmail.com>
2020-04-02 18:46:23 -0400

    still working on isValid, up to sixth draft. One test not passing.

--- string/isValid.js
+++ string/isValid.js
@@ -1,29 +1,247 @@
-///////////// *** FIRST DRAFT *** /////////////
+const s = 'abcdefghhgfedecba' // YES
+// const s = 'aabbcd' // NO
+// const s = 'aabbccddeefghi' // NO
+// const s = 'aabbcc' // YES
+// const s = 'ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd' // YES
+// const s = 'aaaaabc' // NO
+// const s = 'aabbcccdd' // YES
+isValid(s)
+
+///////////// SIXTH DRAFT ///////////////
+
+function isValid(s) {
+  const letters = {}
+  let freqs
+  let max
+  let min
+
+  for (let l of s) {
+    letters[l] ? letters[l]++ : letters[l] = 1
+  }
+
+  freqs = [...new Set(Object.values(letters))]
+
+  if (freqs.length === 1) {
+    console.log('YES')
+    return 'YES'
+  }
+
+  if (freqs.length < 3) {
+    if (freqs[0] > freqs[1]) {
+      max = freqs[0]
+      min = freqs[1]
+    } else if (freqs[1] > freqs[0]) {
+      max = freqs[1]
+      min = freqs[0]
+    }
+  
+    let maxCount = 0
+    let minCount = 0
+
+    for (let l in letters) {
+      if (letters[l] === max) maxCount++
+      if (letters[l] === min) minCount++
+    }
+
+    console.log('maxCount', maxCount, 'minCount', minCount)
+
+    if (maxCount === 1 || minCount === 1) {
+      if (max === 1 || min === 1 || Math.abs(max - min) === 1) {
+        console.log('YES - one deletion available')
+        return 'YES'
+      }
+    }
+  }
+ 
+  console.log(letters)
+  console.log(freqs)
+
+  console.log('NO - too many characters')
+  return 'NO'
+}
+
+
+///////////// *** FIFTH DRAFT *** /////////////
 
 // function isValid(s) {
-//   const hGram = {}
-//   let max = 0
+//   const letters = {}
+//   let freqs
+//   let max
+//   let min
+
+//   for (let l of s) {
+//     letters[l] ? letters[l]++ : letters[l] = 1
+//   }
+
+//   freqs = [...new Set(Object.values(letters))]
+
+//   console.log(letters)
+//   console.log(freqs)
+
+//   if (freqs.length === 1) {
+//     console.log('YES')
+//     return 'YES'
+//   }
+
+//   if (freqs.length < 3) {
+//     if (freqs[0] > freqs[1]) {
+//       max = freqs[0]
+//       min = freqs[1]
+//     } else if (freqs[1] > freqs[0]) {
+//       max = freqs[1]
+//       min = freqs[0]
+//     }
+  
+//     let maxCount = 0
+//     let minCount = 0
+
+//     for (let l in letters) {
+//       if (letters[l] === max) maxCount++
+//       if (letters[l] === min) minCount++
+//     }
+
+//     console.log('maxCount', maxCount, 'minCount', minCount)
+
+//     const static = maxCount > minCount ? max : min
+//     const outlier = maxCount < minCount ? max : min
+
+//     console.log('static =', static, 'outlier =', outlier)
+
+//     if (maxCount !== 1 && minCount !== 1) {
+//       console.log('NO - maxcount nor mincount !== 1')
+//       return 'NO'
+//     }
+
+//     if (outlier === 1 || Math.abs(outlier - 1) === static) {
+//       console.log('YES - outlier')
+//       return 'YES'
+//     }
+    
+
+//     // if (maxCount === 1 || minCount === 1) {
+//     //   if (outlier === 1 || Math.abs(outlier - 1) === static) {
+//     //     console.log('YES - outlier')
+//     //     return 'YES'
+//     //   }
+//     //   if (max === 1 || min === 1 || Math.abs(max - min) === 1) {
+//     //     console.log('YES - one deletion available')
+//     //     return 'YES'
+//     //   }
+//     // }
+//   }
+
+//   console.log('NO - too many characters')
+//   return 'NO'
+// }
+
+
+///////////// *** FOURTH DRAFT *** /////////////
+
+// function isValid(s) {
+//   const letters = {}
+//   let freqs
+//   let diff
+//   let unMatched
 //   let count = 0
 
 //   for (let l of s) {
-//     hGram[l] ? hGram[l]++ : hGram[l] = 1
-//     max = Math.max(hGram[l], max)
+//     letters[l] ? letters[l]++ : letters[l] = 1
 //   }
 
-//   for (let l in hGram) {
-//     if (hGram[l] < max) count++
-//     if (count > 1) {
-//       console.log('NO')
-//       return 'NO'
-//     }
-//     if (hGram[l] <= max - 2) {
-//       console.log('NO')
-//       return 'NO'
+  
+
+//   console.log(letters)
+
+//   freqs = Object.values(letters)
+//   console.log(freqs)
+
+//   const set = new Set(freqs)
+//   console.log(set)
+//   console.log(set.size)
+
+//   diff = (Math.max(...freqs) - Math.min(...freqs))
+
+//   if (diff > 1) {
+//     console.log('NO - diff > 1')
+//     return 'NO'
+//   }
+
+//   for (let i = 1; i < freqs.length; i++) {
+//     if (freqs[0] !== freqs[i]) {
+//       count ++
+//       if (count > 1) {
+//         console.log('NO - count > 1')
+//         return 'NO'
+//       }
+//       unMatched = i
 //     }
 //   }
 
-//   console.log(hGram, max)
+//   console.log(unMatched)
+
+//   if (freqs[unMatched] > 1) {
+//     console.log('NO - unMatched > 1')
+//     return 'NO'
+//   }
+
 //   console.log('YES')
+//   return 'YES'
+// }
+
+
+///////////// *** THIRD DRAFT *** /////////////
+
+// function isValid(s) {
+//   // hash table for count of each character
+//   const letters = {}
+
+//   // reserved variable for array containing character counts
+//   let freqs
+
+//   // reserved variable for difference between least and greatest char counts
+//   let diff
+
+//   //reserved variable for last count in freqs which !== first count
+//   let lastIneq
+
+//   // count of unlike character counts in freq
+//   let count = 0
+
+//   // initialization of character count hash table
+//   for (let l of s) {
+//     letters[l] ? letters[l]++ : letters[l] = 1
+//   }
+
+//   // array containing character counts
+//   freqs = Object.values(letters)
+
+//   // difference between least and greatest char counts
+//   diff = (Math.max(...freqs) - Math.min(...freqs))
+
+//   // if difference is greater than 1: return NO
+//   if (diff > 1) {
+//     console.log('NO - diff > 1')
+//     return 'NO'
+//   }
+
+  
+//   for (let i = 1; i < freqs.length; i++) {
+//     if (freqs[0] !== freqs[i]) {
+//       count ++
+//       if (count > 1) return 'NO - count > 1'
+//       lastIneq = freqs[i]
+//     }
+//   }
+
+//   // if last visited freqs which !== first character > 1: return NO
+//   if (lastIneq > 1) {
+//     console.log('NO - lastIneq')
+//     return 'NO'
+//   }
+
+//   // if vaild: return YES
+//   console.log('YES')
+//   return 'YES'
 // }
 
 
@@ -60,15 +278,30 @@
 // }
 
 
+///////////// *** FIRST DRAFT *** /////////////
 
+// function isValid(s) {
+//   const hGram = {}
+//   let max = 0
+//   let count = 0
 
-const s = 'abcdefghhgfedecba' // YES
-// const s = 'aabbcd' // NO
-// const s = 'aabbccddeefghi' //NO
-isValid(s)
+//   for (let l of s) {
+//     hGram[l] ? hGram[l]++ : hGram[l] = 1
+//     max = Math.max(hGram[l], max)
+//   }
 
-// create histogram => { a: 2, b: 2, c: 2, d: 2, e: 3, f: 2, g: 2, h: 2 }
-// if there are more than two values => return NO
-// if there are two values
-//  if the outlier value minus 1 does not equal other value or 0 => return NO
-// return YES
+//   for (let l in hGram) {
+//     if (hGram[l] < max) count++
+//     if (count > 1) {
+//       console.log('NO')
+//       return 'NO'
+//     }
+//     if (hGram[l] <= max - 2) {
+//       console.log('NO')
+//       return 'NO'
+//     }
+//   }
+
+//   console.log(hGram, max)
+//   console.log('YES')
+// }
