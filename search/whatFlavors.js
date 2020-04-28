5206e140d596ce626a6bb35397e272976b4d35c7
dangrammer <danieljromans@gmail.com>
2020-04-28 14:35:37 -0400

    added optimized solution for ice cream parlor

--- search/whatFlavors.js
+++ search/whatFlavors.js
@@ -1,27 +1,3 @@
-function whatFlavors(cost, money) {
-  const hash = {}
-
-  for (let i = 0; i < cost.length; i++) {
-    hash[cost[i]] ? 
-      hash[cost[i]].push(i + 1) : 
-        hash[cost[i]] = [i + 1]
-  }
-
-  for (let c of cost) {
-    if (hash[money - c]) {
-      if (c === money - c) {
-        if (hash[c].length > 1) {
-          console.log(hash[c][0], hash[c][1])
-          return
-        } 
-      } else {
-        console.log(hash[c][0], hash[money - c][0])
-        return
-      }
-    }
-  }
-}
-
 // const money = 4
 // const cost = [1, 4, 5, 3, 2] // 1 4
 // const money = 4
@@ -29,3 +5,43 @@ function whatFlavors(cost, money) {
 const money = 8
 const cost = [4, 3, 2, 5, 7] // 2 4
 whatFlavors(cost, money)
+
+//////////// *** SECOND DRAFT *** ////////////
+
+function whatFlavors(cost, money) {
+  const hash = {}
+
+  for (let i = 1; i < (cost.length + 1); i++) {
+    const val1 = cost[i - 1] // current element
+    const val2 = money - val1 // target element
+
+    if (hash[val2]) return console.log(hash[val2], i)
+    if (!hash[val1]) hash[val1] = i
+  }
+}
+
+//////////// *** FIRST DRAFT *** ////////////
+
+// function whatFlavors(cost, money) {
+//   const hash = {}
+
+//   for (let i = 0; i < cost.length; i++) {
+//     hash[cost[i]] ? 
+//       hash[cost[i]].push(i + 1) : 
+//         hash[cost[i]] = [i + 1]
+//   }
+
+//   for (let c of cost) {
+//     if (hash[money - c]) {
+//       if (c === money - c) {
+//         if (hash[c].length > 1) {
+//           console.log(hash[c][0], hash[c][1])
+//           return
+//         } 
+//       } else {
+//         console.log(hash[c][0], hash[money - c][0])
+//         return
+//       }
+//     }
+//   }
+// }
