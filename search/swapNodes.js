a9d36560a4b42e2ecd6c8ed2c2f9f8d973cb0051
dangrammer <danieljromans@gmail.com>
2020-05-13 19:10:44 -0400

    started algo solution for swapNodes

---
+++ search/swapNodes.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(depth, index, left, right) {
+    this.depth = depth
+    this.index = index
+    this.left = left
+    this.right = right
+  }
+}
+
+function swapNodes(indexes, queries) {
+  
+}
+
+const indexes = [[2, 3], [-1, -1], [-1 -1]]
+const queries = [1, 1]
+swapNodes(indexes, queries)
+  
