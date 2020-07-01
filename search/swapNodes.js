fbd9beb022b2c69e2aca9b68522b2d875d480427
dangrammer <danieljromans@gmail.com>
2020-06-30 21:13:52 -0400

    added buildTree function to swap nodes solution, still working things out

--- search/swapNodes.js
+++ search/swapNodes.js
@@ -1,11 +1,60 @@
+// const indexes = [[2, 3], [-1, -1], [-1 -1]]
+const indexes = [[2, 3], [-1, 4], [6, -1], [7, 5]]
+const queries = [1, 1]
+// swapNodes(indexes, queries)
+
 class Node {
-  constructor(data, depth, left, right) {
+  constructor(data = -1) {
     this.data = data
-    this.depth = depth
-    this.left = left
-    this.right = right
+    this.left = null
+    this.right = null
   }
 }
+
+function buildTree(indexes) {
+  const queue = []
+  const root = new Node(1)
+  let i = 0
+
+  root.left = new Node()
+  root.right = new Node()
+
+  queue.push([root.left, root.right])
+
+  while (i < indexes.length) {
+    const [left, right] = indexes[i]
+    const [curLeft, curRight] = queue.shift() 
+
+    curLeft.data = left
+    curRight.data = right
+
+    if (left > -1) {
+      curLeft.left = new Node()
+      curLeft.right = new Node()
+      queue.push([curLeft.left, curLeft.right])
+    }
+    
+
+    if (right > -1) {
+      curRight.left = new Node()
+      curRight.right = new Node()
+      queue.push([curRight.left, curRight.right])
+    }
+
+    i++
+  }
+  return root
+}
+
+// function printNodes(root) {
+//   if (root && root.data > -1) {
+//     printNodes(root.left) 
+//     console.log(root.data) 
+//     printNodes(root.right) 
+//   }
+// }
+
+// printNodes(buildTree(indexes))
 
 function inOrderTraversal(indexes) {
   
@@ -16,8 +65,3 @@ function swapNodes(indexes, queries) {
 
   return result
 }
-
-const indexes = [[2, 3], [-1, -1], [-1 -1]]
-const queries = [1, 1]
-swapNodes(indexes, queries)
-  
